1c41b6f376ddbb094129bf368f3aace24f939e14
ibayckd <93259072+ibayckd@users.noreply.github.com>
2022-09-10 10:44:42 +0700

    Add files via upload

---
+++ learntwoo.py
@@ -0,0 +1,20 @@
+# Biodata saya, iqbal ferizal
+
+nama = "makhluk hidup"
+sekolah = "Bumi"
+kelas = 10
+jurusan = "IPA"
+rombel = 7
+umur = 199
+alamat = "Jalan Umum Nomor 01"
+
+# Nama          : Makhluk hidup (199)
+# Sekolah       : Bumi
+# Kelas         : 10 IPA 7
+# Alamat        : Jalan Umum
+
+
+print("Nama\t: " + nama + " (" + str(umur) + ")")
+print("Sekolah\t: "+(sekolah))
+print("Kelas\t: " + str(kelas) + " (" + str(jurusan)+") " + str(rombel))
+print("Alamat\t: "+str(alamat))
